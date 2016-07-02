470f34411423bfdc3426f398ba9959de1347d21b
Jason Symons <jason.symons@gmail.com>
2016-07-02 16:18:00 -0400

    First FT specced out in comments, and now uses unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,54 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import unittest
 
+# Make selenium use the marionette driver and firefox developer edition
 caps = DesiredCapabilities.FIREFOX
 caps["marionette"] = True
 caps["binary"] = "/usr/bin/firefox-aurora"
 
-browser = webdriver.Firefox(capabilities=caps)
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox(capabilities=caps)
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Edith has heard about a cool new online to-do app.
+		# She goes to check out its homepage
+		self.browser.get('http://localhost:8000')
+
+		# She notices the page title and header mention
+		# to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+		# She is invited to enter a to-do item straight away
+
+		# She types "Buy peacock feathers" into a text box
+		# (Edith's hobby is tying fly-fising lures)
+
+		# When she hits enter, the page updates, and now the page lists
+		# "1: Buy peacock feathers" as an item in a to-do list
+
+		# There is still a text box inviting her to add another item.
+		# She enters "Use peacock feathers to make a fly"
+		# (Edith is very methodical)
+
+		# The page updates again, and now shows both items on her list
+
+		# Edith wonders whether the site will remember he list. The she
+		# sees that the site has generated a unique URL for her
+		# -- there is some explanatory text to that effect.
+
+		# She visits that URL - her to-do list is still there.
+
+		# Satisfied she goes back to sleep
+
+		browser.quit()
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
